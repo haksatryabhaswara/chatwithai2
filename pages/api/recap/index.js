c5554b91020c80902ad2bfaab4782d336e974fa2
haksatryabhaswara <windstory22@gmail.com>
2021-12-18 18:41:16 +0700

    console log captcha 3#

--- pages/api/recap/index.js
+++ pages/api/recap/index.js
@@ -23,40 +23,44 @@ export default function handler(req, res) {
           .then((response) => response.json())
           .then((dataCapGoogle) => {
             console.log(dataCapGoogle);
-          });
-        const data = {
-          prompt: req.body.prompt,
-          temperature: 0.9,
-          max_tokens: 100,
-          top_p: 1.0,
-          frequency_penalty: 1,
-          presence_penalty: 0.6,
-          stop: ["\\n", " Human:", " AI:"],
-        };
-        fetch("https://api.openai.com/v1/engines/ada/completions", {
-          body: JSON.stringify(data),
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${process.env.OAK}`,
-          },
-          method: "POST",
-        })
-          .then((response) => response.json())
-          .then((dataRes) => {
-            // console.log(dataRes);
-            res.statusCode = 200;
-            res.setHeader("Content-Type", "application/json");
-            res.setHeader(
-              "Cache-Control",
-              "public, max-age=20140, must-revalidate, no-transform"
-            );
-            res.end(JSON.stringify(dataRes));
-            resolve();
-          })
-          .catch((error) => {
-            res.json(error);
-            res.status(405).end();
-            return resolve();
+            if (dataCapGoogle.success == true) {
+              const data = {
+                prompt: req.body.prompt,
+                temperature: 0.9,
+                max_tokens: 100,
+                top_p: 1.0,
+                frequency_penalty: 1,
+                presence_penalty: 0.6,
+                stop: ["\\n", " Human:", " AI:"],
+              };
+              fetch("https://api.openai.com/v1/engines/ada/completions", {
+                body: JSON.stringify(data),
+                headers: {
+                  "Content-Type": "application/json",
+                  Authorization: `Bearer ${process.env.OAK}`,
+                },
+                method: "POST",
+              })
+                .then((response) => response.json())
+                .then((dataRes) => {
+                  // console.log(dataRes);
+                  res.statusCode = 200;
+                  res.setHeader("Content-Type", "application/json");
+                  res.setHeader(
+                    "Cache-Control",
+                    "public, max-age=20140, must-revalidate, no-transform"
+                  );
+                  res.end(JSON.stringify(dataRes));
+                  resolve();
+                })
+                .catch((error) => {
+                  res.json(error);
+                  res.status(405).end();
+                  return resolve();
+                });
+            } else {
+              res.status(405).json({ name: "User unauth!" });
+            }
           });
       }
     }
