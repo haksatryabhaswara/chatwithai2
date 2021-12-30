fd8dedacae0b3233e2b474944e760e41e84fa1f2
haksatryabhaswara <windstory22@gmail.com>
2021-12-30 13:18:20 +0700

    eliminasi register/login

--- config/firebaseAuthUI.config.js
+++
@@ -1,9 +0,0 @@
-export const uiConfig = ({ firebase, auth }) => {
-  return {
-    signInFlow: "popup",
-    signInSuccessUrl: "/",
-    tosUrl: "/terms-of-service",
-    privacyPolicyUrl: "/privacy-policy",
-    signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
-  };
-};
