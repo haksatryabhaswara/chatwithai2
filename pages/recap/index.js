339dce84811064d6528880e5f56567003241147a
haksatryabhaswara <windstory22@gmail.com>
2022-01-17 09:34:22 +0700

    version update

--- pages/recap/index.js
+++ pages/recap/index.js
@@ -350,7 +350,7 @@ export default function Home({ siteKey }) {
                 </div>
                 <div className="flex justify-center items-center mt-2">
                   <b className="font-poppins text-gray text-xs text-center">
-                    Version 0.4.2
+                    Version 0.4.5
                   </b>
                 </div>
               </div>
