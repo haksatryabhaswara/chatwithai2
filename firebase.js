8d7df089a4a64966e8875601f6688a8e0e7fed7a
haksatryabhaswara <windstory22@gmail.com>
2022-04-29 01:31:30 +0700

    API block

--- firebase.js
+++ firebase.js
@@ -3,12 +3,12 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const config = {
-  apiKey: "AIzaSyBj4wYsuqNlrOIqg-_f6gPP8_FaK3r79zk",
+  apiKey: process.env.APIFirebase,
   authDomain: "tokkuai.firebaseapp.com",
   projectId: "tokkuai",
   storageBucket: "tokkuai.appspot.com",
   messagingSenderId: "472655829189",
-  appId: "1:472655829189:web:81f8aabf3a4769dd20f8ac",
+  appId: process.env.AppIDFirebase,
   measurementId: "G-78P2ERF9M7",
 };
 
